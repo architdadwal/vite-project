85dbea9691ffbf84eaef4aa8ef0c7d03a0114a87
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2023-12-07 18:44:43 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Background from "./components/Background"
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-800'>
+<Background/>
+    </div>
+  )
+}
+
+export default App
